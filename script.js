5aaa3a6a2fdddb43777ee83d7a6d61519874f95e
timothymichaelcook <tmc295@email.vccs.edu>
2023-03-12 20:33:42 -0400

    declared hourUpdater function, gets current day and time via day js package

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
+  $('.saveBtn').on('click', function() {
+
+    var value = $(this).siblings('description').val();
+    var time = $(this).parent().attr('id');
+    localStorage.setItem(time, value);
+    $('notification').addClass('show');
+    setTimeout(function() {
+      $('.notification').removeClass('show')
+    }, 5000);
+  });
+
+  function hourUpdater() {
+    var currentHour = dayjs().hour();
+    
+  
+  
+  
+  
+  
+  
+  
+  }
 
 });
