22208a5893ef13410246b9fc257a08e08e9df38e
timothymichaelcook <tmc295@email.vccs.edu>
2023-03-12 21:07:19 -0400

    added 17th hour in local storage fetch

--- script.js
+++ script.js
@@ -42,6 +42,7 @@ $(document).ready(function() {
   $('#hour-14 .description').val(localStorage.getItem('hour-14'));
   $('#hour-15 .description').val(localStorage.getItem('hour-15'));
   $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
 
   $('#currentDay').text(dayjs().format('dDdd, MMMM D, YYYY'))
