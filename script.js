78aefcac72b82bfe93732dc909369fdb79fc93b0
timothymichaelcook <tmc295@email.vccs.edu>
2023-03-12 21:06:39 -0400

    finsished loading local storage data for all hours

--- script.js
+++ script.js
@@ -38,24 +38,11 @@ $(document).ready(function() {
   $('#hour-10 .description').val(localStorage.getItem('hour-10'));
   $('#hour-11 .description').val(localStorage.getItem('hour-11'));
   $('#hour-12 .description').val(localStorage.getItem('hour-12'));
-  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
 
 
-    
-    
-  
-  
-  
-  
-  
-  
-  
-  }
-
+  $('#currentDay').text(dayjs().format('dDdd, MMMM D, YYYY'))
 });
