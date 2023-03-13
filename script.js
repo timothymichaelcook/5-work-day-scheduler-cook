5f45b35d2df3f65db58460003c8705f32b625a03
timothymichaelcook <tmc295@email.vccs.edu>
2023-03-12 20:41:48 -0400

    created conditional comparing blockHour var with currectHour var and changing class based on result of condition using if, else if, else syntax

--- script.js
+++ script.js
@@ -12,6 +12,25 @@ $(document).ready(function() {
 
   function hourUpdater() {
     var currentHour = dayjs().hour();
+
+    $('.time-block').each(function() {
+      var blockHour = parseInt($(this).attr('id').split('-')[1]);
+      
+      if (blockHour  < currentHour) {
+        $(this).addClass('past');
+      } else if (blockHour === currentHour) {
+        $(this).removeClass('past');
+        $(this).addClass('present');
+      } else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+      }
+    });;
+  }
+    //Conditional statement checking hour
+
+    
     
   
   
